78273ef4fdd66e3f5ca5082fc05b615c387b7c4f
CHOIJUNHYUK <hyuk.choi@petpeotalk.com>
2023-02-12 21:59:53 +0900

    fail: remoteStream 연결 안 됨

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
+import {registerGlobals} from 'react-native-webrtc';
 import App from './App';
 import {name as appName} from './app.json';
+
+registerGlobals();
 
 AppRegistry.registerComponent(appName, () => App);
